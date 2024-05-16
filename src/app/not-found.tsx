f265bd39a71b72f3f3e99b853a596c03b6be98f1
JustNative <hamzahamdaoui096@gmail.com>
2024-05-16 14:23:38 +0100

    add NotFound page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import EmptyState from '@/components/EmptyState'
+
+export default function NotFound() {
+    return (
+        <EmptyState
+            title='Not Found'
+            subtitle='Something went wrong!'
+        />
+    )
+}
